854ef2093558aae0d7c2e93315018c6022ad7eed
Clark WANG <dearvoid@gmail.com>
2021-03-29 17:23:07 +0800

    add test for v2.3.5

---
+++ tests/spawn-zombie-idle_02.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This is to verify v2.3.5
+
+source $SRCDIR/tests/common.sh || exit 1
+
+assert_run sexpect sp -zombie-ttl 10 sleep 10
+pid=$( sexpect get -pid )
+run sleep 11
+
+# zombie should still be walking around
+assert_run kill -0 $pid
+
+for i in {1..11}; do
+    run sleep 1
+    assert_run sexpect ex -eof
+done
+
+run sleep 11
+negass_run sexpect get
