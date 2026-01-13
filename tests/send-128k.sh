a7f7d9747787f382db5fe59f87ce8219c7b1226c
Clark Wang <dearvoid@gmail.com>
2026-01-13 13:16:12 +0800

    fix "Argument list too long" issue in github workflow tests

--- tests/send-128k.sh
+++ tests/send-128k.sh
@@ -12,7 +12,27 @@ x1k=$( str_repeat x 1024 )
 x128k=$( str_repeat $x1k 128 )
 md5=3832e28c8feea48397f30d70b43d7987
 
-assert_run sexpect s -cr "printf '%s' $x128k | md5sum"
+# In github workflow's env, sending 128k long command would cause error:
+#
+#   /home/runner/work/sexpect/sexpect/tests/common.sh: line 44: /home/runner/work/sexpect/sexpect/build/sexpect: Argument list too long
+#
+# So we should write the 128k data to a tmp file.
+#
+if (( 0 )); then
+    assert_run sexpect s -cr "printf '%s' $x128k | md5sum"
+else
+    tmpfile=/tmp/data
+    true > $tmpfile
+    for ((i = 0; i < 128; ++i)); do
+        printf '%s' $x1k >> $tmpfile
+    done
+
+    #assert_run sexpect s -cr "printf '%s' $x128k | md5sum"
+    assert_run sexpect s "printf '%s' "
+    assert_run sexpect s -f $tmpfile
+    assert_run sexpect s -cr " | md5sum"
+fi
+
 assert_run sexpect ex -i $md5
 assert_run sexpect ex -re "$re_ps1"
 
